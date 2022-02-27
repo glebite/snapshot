56252ba2c2c443ac5170b250edd7992a013b13c7
glebite <glebite@gmail.com>
2022-02-26 21:04:24 -0500

    Defining things - sorting out things...

--- src/snapshot.py
+++ src/snapshot.py
@@ -1,14 +1,23 @@
 """snapshot.py
-"""
 
+Assumptions:
+- the remote system has ssh access
+
+Definitions:
+runner-shell - the system that is running snapshot
+remote-host - the remote system that contains the folder to backup
+
+
+"""
 
 class Snapshot:
     """Snapshot
     """
 
-    def __init__(self):
+    def __init__(self, ip_address, port, username, password):
         """
         """
+        pass
 
 
 def main():
